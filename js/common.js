4e12cd95719ba5b775841c94b9e4eb617af4aca0
PavelKizernis <pashakiz@gmail.com>
2015-12-18 17:26:37 +0300

    add responsive design

--- js/common.js
+++ js/common.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
 
-	// Placeholder
-	$(".search_field").on("focus", function() {
-		if( $(this).val() == "Поиск по сайту" || $(this).val() == "" ) {
-			$(this).val("");
+	/* Placeholder
+		Пример вызова:
+		placeholder(".feedback__name");
+		placeholder(".feedback__email");
+		в html необходимо указать атрибут value
+		<input class="feedback__name" type="text" name="name" value="Имя Фамилия">
+		<input class="feedback__email" type="text" name="email" value="E-mail">
+	*/
+	placeHolder(".search_field");
+	function placeHolder(selector) {
+		var title = "";
+		title = $(selector).val();
+		if (title == "") {
+			return false;
 		}
-	});
-	$(".search_field").on("blur", function() {
-		if( $(this).val() == "" ) {
-			$(this).val("Поиск по сайту");
-		}
+		$(selector).on("focus", function() {
+			if( $(this).val() == title || $(this).val() == "" ) {
+				$(this).val("");
+			}
+		});
+		$(selector).on("blur", function() {
+			if( $(this).val() == "" ) {
+				$(this).val(title);
+			}
+		});
+	}
+
+	$(".btn-mobile-nav").on("click", function(){
+		$(".nav-wrap .nav").toggleClass("show");
 	});
 
 	$('.readmore').on('click', function(){
@@ -27,39 +46,39 @@ $(document).ready(function() {
 	//Parallax (Stellar)
 	//Документация: http://markdalgleish.com/projects/stellar.js/docs/
 	//<div class="image" data-stellar-background-ratio="0">...</div>
-	$.stellar({
-		horizontalScrolling: false,
-		verticalOffset: 0
-	});
+	// $.stellar({
+	// 	horizontalScrolling: false,
+	// 	verticalOffset: 0
+	// });
 	
 	//Попап менеджер FancyBox
 	//Документация: http://fancyapps.com/fancybox/
 	//<a class="fancybox" rel="group" href="big_image_1.jpg"><img src="small_image_1.jpg" alt="" /></a>
 	//<a class="fancybox" rel="group" href="big_image_2.jpg"><img src="small_image_2.jpg" alt="" /></a>
-	$(".fancybox").fancybox();
+	// $(".fancybox").fancybox();
 
 	//Навигация по Landing Page
 	//$(".top_mnu") - это верхняя панель со ссылками.
 	//Ссылки вида <a href="#contacts">Контакты</a>
-	$(".top_mnu").navigation();
+	//$(".top_mnu").navigation();
 
 	//Добавляет классы дочерним блокам .block для анимации
 	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
+	// $(".block").waypoint(function(direction) {
+	// 	if (direction === "down") {
+	// 		$(".class").addClass("active");
+	// 	} else if (direction === "up") {
+	// 		$(".class").removeClass("deactive");
+	// 	};
+	// }, {offset: 100});
 
 	//Плавный скролл до блока .div по клику на .scroll
 	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
-			offset: -90
-		});
-	});
+	// $("a.scroll").click(function() {
+	// 	$.scrollTo($(".div"), 800, {
+	// 		offset: -90
+	// 	});
+	// });
 
 	//Каруселька
 	//Документация: http://www.owlcarousel.owlgraphic.com/docs/started-welcome.html
@@ -75,20 +94,19 @@ $(document).ready(function() {
 	$(".owl-carousel-news").owlCarousel({
 		items: 1,
 		dots: true,
-		loop: true,
-		autoplay: true
+		loop: true //,		autoplay: true
 	});
 
 	//Кнопка "Наверх"
 	//Документация:
 	//http://api.jquery.com/scrolltop/
 	//http://api.jquery.com/animate/
-	$("#top").click(function () {
-		$("body, html").animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
+	// $("#top").click(function () {
+	// 	$("body, html").animate({
+	// 		scrollTop: 0
+	// 	}, 800);
+	// 	return false;
+	// });
 	
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
